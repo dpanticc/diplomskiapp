78e32240c3947a2b0f4f463cddb69773d314f46b
Dušan Pantić <dusanpan@telekom.rs>
2024-01-23 08:14:16 +0100

    Committing changes in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,27 @@ import { AuthGuard } from './guards/auth.guard';
 import { HomeGuard } from './guards/home.guard';
 import { RoleGuard } from './guards//role.guard.';
 import { UserGuard } from './guards//user.guard';
+import { UsersComponent } from './components/users/users.component';
+import { ReservationsComponent } from './components/reservations/reservations.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
 
 export const routes: Routes = [
 
 
   {path: '', component: LoginComponent, canActivate:[HomeGuard]},
   {path: 'home', component: HomeComponent, canActivate:[AuthGuard, UserGuard]},
-  {path: 'admin-home', component: AdminComponent, canActivate:[AuthGuard, RoleGuard]}  
+  {
+    path: 'admin-home',
+    component: AdminComponent,
+    children: [
+      { path: 'users', component: UsersComponent },
+      { path: 'rooms', component: RoomsComponent },
+      { path: 'reservations', component: ReservationsComponent },
+    ],
+  },
+
+
+
 
 ];
 
